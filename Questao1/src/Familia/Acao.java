076f8e66badc7e1b397f61d0e8576386c160b5ef
Kimberly Scaldaferro Colodeti <103203864+KimberlyScaldaC@users.noreply.github.com>
2025-03-19 15:34:56 -0300

    Update Acao.java

--- Questao1/src/Familia/Acao.java
+++ Questao1/src/Familia/Acao.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-//package Familia;
+package Familia;
 
 /**
  *
@@ -14,28 +14,12 @@ public class Acao extends Thread {
   private int valorSaque;
   private int tempo;
 
-  // private int totalGastadora = 0;
-  // private int totalEsperta = 0;
-  // private int totalEconomica = 0;
-
   public Acao(String nomeThread, int tempo, int valorSaque, Conta conta) {
     super(nomeThread);
     this.tempo = tempo;
     this.valorSaque = valorSaque;
     this.conta = conta;
   }
-
-  // public int getTotalGastadora() {
-  // return totalGastadora;
-  // }
-
-  // public int getTotalEsperta() {
-  // return totalEsperta;
-  // }
-
-  // public int getTotalEconomica() {
-  // return totalEconomica;
-  // }
 
   public void imprimirResumo() {
 
@@ -46,46 +30,60 @@ public class Acao extends Thread {
     System.out.println("           -> Quantidade de Saques: " + conta.getQuantEsperta());
     System.out.println("\nAEconomica -> sacou um total de R$ " + conta.getTotalEconomica() + ",00");
     System.out.println("           -> Quantidade de Saques: " + conta.getQuantEconomica());
-    System.out.println("\n----------------------------------");
-    System.exit(0);
+    System.out.println("\nAPatrocinadora -> sacou um total de R$ " + conta.getTotalPatrocinadora() + ",00");
+    System.out.println("           -> Quantidade de Saques: " + conta.getQuantPatrocinadora());
+    System.out.println("\n----------------------------------\n");
+    // System.exit(0);
   }
 
   public void run() {
-    int total = 0;
+
     while (true) {
-
-      if (conta.getSaldo() == 0) {
-        imprimirResumo();
-
-      }
-
+      int total = 0;
       try {
-        // Espera tempo milissegundos
         Thread.sleep(tempo);
-
       } catch (InterruptedException e) {
         e.printStackTrace();
       }
 
-      if (conta.getSaldo() >= valorSaque) {
-        if (getName() == "AGastadora") {
-          total = valorSaque + conta.getTotalGastadora();
-          conta.setTotalGastadora(total);
-          // System.out.println(" ENTROU 10 -> " + conta.getTotalGastadora());
+      synchronized (conta) { // Lock na conta
+        // Se o saldo for 0, imprime o resumo e faz o depósito
+        if (conta.getSaldo() == 0) {
+          if (getName() == "APatrocinadora") {
+            conta.Deposito(valorSaque, getName()); // Deposita o valor
+            total = valorSaque + conta.getTotalPatrocinadora();
+            conta.setTotalPatrocinadora(total);
+            imprimirResumo(); // Imprime o resumo
+            conta.notifyAll(); // Libera todas as threads bloqueadas
+
+          }
+          // break; // Encerra a thread
         }
-        if (getName() == "AEsperta") {
-          total = valorSaque + conta.getTotalEsperta();
-          conta.setTotalEsperta(total);
-          // System.out.println(" ENTROU 10 -> " + conta.getTotalEsperta());
+
+        // Verifica se pode sacar
+        if (conta.getSaldo() >= valorSaque) {
+          if (getName() == "AGastadora") {
+            total = valorSaque + conta.getTotalGastadora();
+            conta.setTotalGastadora(total);
+            // System.out.println(" ENTROU 10 -> " + conta.getTotalGastadora());
+          }
+          if (getName() == "AEsperta") {
+            total = valorSaque + conta.getTotalEsperta();
+            conta.setTotalEsperta(total);
+            // System.out.println(" ENTROU 10 -> " + conta.getTotalEsperta());
+          }
+          if (getName() == "AEconomica") {
+            total = valorSaque + conta.getTotalEconomica();
+            conta.setTotalEconomica(total);
+            // System.out.println(" ENTROU 10 -> " + conta.getTotalEconomica());
+          }
         }
-        if (getName() == "AEconomica") {
-          total = valorSaque + conta.getTotalEconomica();
-          conta.setTotalEconomica(total);
-          // System.out.println(" ENTROU 10 -> " + conta.getTotalEconomica());
+        if (getName() != "APatrocinadora") {
+          conta.saque(valorSaque, getName());
         }
       }
 
-      conta.saque(valorSaque, getName());
+      // conta.saque(valorSaque, getName());
 
     }
 
