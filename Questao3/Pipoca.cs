ae6b97d56ece6de5cb3bdd05611eaf68c478e684
Kimberly Scaldaferro Colodeti <103203864+KimberlyScaldaC@users.noreply.github.com>
2025-03-26 07:42:10 -0300

    Delete Questao3 directory

--- Questao3/Pipoca.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Threading.Tasks;
-
-public class Pipoca {
-    public async Task<string> GetPipocaAsync() {
-        await Task.Delay(2000); 
-        return "Pipoca Pronta";
-    }
-}
--- Questao3/Refrigerante.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Threading.Tasks;
-
-public class Refrigerante {
-    public async Task<string> GetRefrigeranteAsync() {
-        await Task.Delay(1500); 
-        return "Refrigerante Pronto";
-    }
-}
