1f71ece834b646f3088a580101725478945e0dbc
Vito <103535367+OCVitin@users.noreply.github.com>
2025-03-26 07:53:43 -0300

    Update main.go

--- Questão 2/main.go
+++ Questão 2/main.go
@@ -1,5 +1,6 @@
 package main
 
+
 import (
 	"encoding/csv"
 	"fmt"
@@ -10,7 +11,7 @@ import (
 	"sync"
 )
 
-// Primeiro de tudo, construímos uma classe com todas as informações necessárias para cadastrar um contato.
+// Primeiro de tudo, construímos uma estrutura com todas as informações necessárias para cadastrar um contato.
 type Aluno struct {
 	Matricula string
 	Nome      string
@@ -27,28 +28,29 @@ func limpaConsole() {
 
 // Essa é a nossa função main, que chama diretamente o menu.
 func main() {
+	// Array com os nomes dos arquivos a serem chamados
 	arrArquivos := []string{"administração.csv", "arquitetura.csv", "biologia.csv", "computacao.csv", "direito.csv", "economia.csv", "engenharia.csv", "filosofia.csv",
 				"física.csv", "história.csv", "letras.csv", "matemática.csv", "medicina.csv", "psicologia.csv", "química.csv"}
 	limpaConsole()
 
-	var wg sync.WaitGroup
+	var wg sync.WaitGroup // Ferramenta da biblioteca do Go para promover sincronismo entre funções
 	for _, arquivo := range arrArquivos {
 		// Adiciona 1 ao contador do WaitGroup para cada goroutine
 		wg.Add(1)
 
 		// Inicia a goroutine
-		go buscaCompleto(arquivo, &wg)
+		go buscaCompleto(arquivo, &wg) // Passa o nome do arquivo como parâmetro
 	}
 
 	// Espera todas as goroutines terminarem antes de continuar
 	wg.Wait()
 
 	fmt.Println("Todos os arquivos foram processados.")
-	// menu()
 }
 
+// Função que busca os alunos com curso COMPLETO
 func buscaCompleto(nomeArquivo string, wg *sync.WaitGroup) {
-	defer wg.Done()
+	defer wg.Done() // Reduz o contador do WaitGroup em 1 quando a função terminar de ser executada
 
 	// Abre o arquivo CSV
 	file, err := os.Open(nomeArquivo)
@@ -60,7 +62,7 @@ func buscaCompleto(nomeArquivo string, wg *sync.WaitGroup) {
 	// Cria um novo leitor CSV
 	reader := csv.NewReader(file)
 
-	// Lê a primeira linha para ignorar os cabeçalhos
+	// Lê a primeira linha para ignorar os cabeçalhos (Matrícula/Nome/Curso/Cursando)
 	_, err = reader.Read()
 	if err != nil {
 		log.Fatal("Erro ao ler os cabeçalhos do arquivo:", err)
@@ -75,13 +77,14 @@ func buscaCompleto(nomeArquivo string, wg *sync.WaitGroup) {
 	// Processa cada linha do CSV
 	for _, record := range records {
 		if len(record) == 4 {
-			// Converte o valor 'Cursando' para booleano
+			// Converte o valor 'Cursando' para booleano (sim para cursando ou não para completo)
 			flag, err := strconv.ParseBool(record[3])
 			if err != nil {
 				log.Printf("Erro ao converter 'Cursando' para bool na linha %+v: %v\n", record, err)
 				continue
 			}
 
+			// Cria uma instância de Aluno conforme os dados oferecidos pela leitura dos arquivos CSV
 			aluno := Aluno{
 				Matricula: record[0],
 				Nome:      record[1],
@@ -89,7 +92,8 @@ func buscaCompleto(nomeArquivo string, wg *sync.WaitGroup) {
 				Flag:      flag,
 			}
 
-			// Imprime as informações do aluno
+			// Imprime as informações do aluno linearmente (tentei fazer um dado do aluno para cada linha,
+			// mas as funções sendo executadas concorrentemente estava quebrando e deixando o output confuso)
 			if !aluno.Flag {
 				fmt.Printf("%s %s %s COMPLETO\n", aluno.Matricula, aluno.Nome, aluno.Curso)
 			}
